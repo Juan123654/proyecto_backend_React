bdfc2db8ca4df107237a9643700f00c3b77b64a4
Juan123654 <jplsslon@hotmail.com>
2024-04-18 20:40:47 -0500

    cambio de puerto 3300 para conectar desde local

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/consulta', function(req, res){
     res.sendFile(dir_front + "/frontend/build/index.html")
 });
 
-app.listen(8080,()=>{
+app.listen(3300,()=>{
     console.log("Server Started");
 });
